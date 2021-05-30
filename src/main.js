44bd20fa93d28c6d19f7c1ae6c0c74c40d4b1f9c
josemasf <josemasf@gmail.com>
2021-05-30 13:27:41 +0200

    add vuetifly

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import vuetify from './plugins/vuetify'
 
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#vue-login')
